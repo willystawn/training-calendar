b8dc1999609d880b0026425d9e0f4a49657dfca3
Willy Setiawan <willystawn@gmail.com>
2025-08-11 11:37:33 +0700

    Refactor code structure for improved readability and maintainability

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 import { Database } from './database.types';
 
 // Ambil credential dari environment variable
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL as string;
+const supabaseAnonKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
